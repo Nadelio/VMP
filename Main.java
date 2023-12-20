e126161872eeeb002f36e8366f2c30a60ec74527
Nadelio <carefulsniffle@gmail.com>
2023-12-20 12:27:01 -0600

    ... Fixing things and removing unused methods/imports/etc.

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JFrame;
